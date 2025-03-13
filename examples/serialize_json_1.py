47ab7d9ff90199d508d4650021a53b499ed43d2b
Rob <mail@zaufall.de>
2025-03-13 20:56:59 +0100

    clean code

--- examples/serialize_json_1.py
+++ examples/serialize_json_1.py
@@ -5,8 +5,5 @@ import mindmap.serialization as mms
 document = mm.MindmapDocument()
 document.get_mindmap()
 
-guid_mapping = {}
-mms.build_mapping(document.mindmap, guid_mapping)
-
 serialize_simple_result = mms.serialize_object_simple(document.mindmap)
 print(json.dumps(serialize_simple_result, indent=1))
