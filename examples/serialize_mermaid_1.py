862aac0a50e618d6c43d09a75757cd83a36fe0a4
Rob <mail@zaufall.de>
2025-03-13 20:57:42 +0100

    add visual test step to mermaid serialization examples

--- examples/serialize_mermaid_1.py
+++ examples/serialize_mermaid_1.py
@@ -1,5 +1,6 @@
 import mindmap.mindmap as mm
 import mindmap.serialization as mms
+import json
 
 document = mm.MindmapDocument()
 document.get_mindmap()
@@ -9,3 +10,6 @@ mms.build_mapping(document.mindmap, guid_mapping)
 
 mermaid_id_only = mms.serialize_mindmap(document.mindmap, guid_mapping, id_only=True)
 print(mermaid_id_only)
+
+deserialized = mms.deserialize_mermaid_with_id(mermaid_id_only, guid_mapping)
+print(json.dumps(mms.serialize_object_simple(deserialized), indent=1))
--- examples/serialize_mermaid_2.py
+++ examples/serialize_mermaid_2.py
@@ -1,5 +1,6 @@
 import mindmap.mindmap as mm
 import mindmap.serialization as mms
+import json
 
 document = mm.MindmapDocument()
 document.get_mindmap()
@@ -9,3 +10,6 @@ mms.build_mapping(document.mindmap, guid_mapping)
 
 mermaid_data = mms.serialize_mindmap(document.mindmap, guid_mapping, id_only=False)
 print(mermaid_data)
+
+deserialized = mms.deserialize_mermaid_full(mermaid_data, guid_mapping)
+print(json.dumps(mms.serialize_object_simple(deserialized), indent=1))
